b5f61477588439ba8b65d721861170338364b3ec
fredrikkluft <s309293@stud.hioa.no>
2018-01-31 15:39:14 +0100

    update in class udp server

--- src/LabN3/MultiClientServerUDP.java
+++ src/LabN3/MultiClientServerUDP.java
@@ -1,66 +1,28 @@
 package LabN3;
+
 import java.net.*;
 import java.io.*;
 
-public class MultiClientServerUDP
-{
+public class MultiClientServerUDP {
 
     private static int serverPort = 25565;
     private CurrencyConverter converter;
     private QueryParser parser;
 
-    public MultiClientServerUDP()
-    {
+    public MultiClientServerUDP() {
         this.converter = new CurrencyConverter("kursliste.csv");
         this.parser = new QueryParser();
         runServer();
     }
 
     private void runServer() {
+        try (
+                // create an UDP/datagram socket for server on the given port
+                DatagramSocket serverSocket = new DatagramSocket(serverPort)
 
-        try {
-
-            // creates an UDP/datagram socket for server on the given port
-            DatagramSocket serverSocket = new DatagramSocket(serverPort);
-
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-            System.err.println("Something went wrong :/");
-        }
-
-        }
-
-
-
-    public static void main(String[] args) throws IOException
-    {
-
-        int portNumber = 5555; // Default port to use
-
-        if (args.length > 0)
-        {
-            if (args.length == 1)
-                portNumber = Integer.parseInt(args[0]);
-            else
-            {
-                System.err.println("Usage: java EchoUcaseServerUDP [<port number>]");
-                System.exit(1);
-            }
-        }
-
-        System.out.println("Hi, I am EchoUCase UDP server!");
-
-        try
-                (
-                        // create an UDP/datagram socket for server on the given port
-                        DatagramSocket serverSocket =
-                                new DatagramSocket(portNumber);
-                )
-        {
+        ) {
             String receivedText;
-            do
-            {
+            do {
                 byte[] buf = new byte[1024];
 
 
@@ -84,7 +46,7 @@ public class MultiClientServerUDP
                 InetAddress clientAddr = packet.getAddress();
                 int clientPort = packet.getPort();
 
-                System.out.println("Client [" + clientAddr.getHostAddress() +  ":" + clientPort +"] > " + receivedText);
+                System.out.println("Client [" + clientAddr.getHostAddress() + ":" + clientPort + "] > " + receivedText);
 
                 // create datagram packet with the uppercase text to send back to the client
                 packet = new DatagramPacket(buf, buf.length, clientAddr, clientPort);
@@ -92,17 +54,39 @@ public class MultiClientServerUDP
                 // send the uppercase text back to the client
                 serverSocket.send(packet);
 
-                System.out.println("I (Server) [" + InetAddress.getLocalHost() + ":" + portNumber + "] > " + outText);
+                System.out.println("I (Server) [" + InetAddress.getLocalHost() + ":" + serverPort + "] > " + outText);
             } while (receivedText != null);
 
             System.out.println("I am done, Bye!");
 
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
-                    + portNumber + " or listening for a connection");
+                    + serverPort + " or listening for a connection");
             System.out.println(e.getMessage());
         }
     }
 
+    private static class Client extends Thread {
+        private Socket socket;
+        private String address;
+
+        private Client(Socket socket) {
+            this.socket = socket;
+            this.address = socket.getInetAddress().getHostAddress();
+            System.out.println("New client [" + address + "] connected");
+        }
+
+
+        public static void main(String[] args) throws IOException {
+
+            if (args.length == 1)
+                serverPort = Integer.parseInt(args[0]);
+            else if (args.length > 1)
+                throw new IllegalArgumentException("Usage: SingleClientServer [<serverPort number>]");
+
+            new MultiClientServerUDP();
+
+
+        }
+    }
 }
